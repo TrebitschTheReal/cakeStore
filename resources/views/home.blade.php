0fcde37fd161488459c63cd38807f6e09a2b5c1c
Viktor Dohanyos <vorkapp@gmail.com>
2020-03-22 11:40:40 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,6 @@
                 <div class="card-body">
                     @foreach($cakes as $cake)
                         Torta neve: <h3>{{ $cake->name }}</h3>
-
                         @foreach($cake->required_ingredients as $ingredient)
                             <p>Alapanyag: {{$ingredient->name}}</p>
                             <p>Mennyi kell: {{$ingredient->pivot->ingredient_quantity}} egység</p>
@@ -30,6 +29,55 @@
                         @endforeach
                         <hr>
                     @endforeach
+                </div>
+            </div>
+            <div class="card mt-3">
+                <div class="card-header">Torta felvétele</div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Neve') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Leírása') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Szükséges alapanyag') }}</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control" name="" id="">
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Mennyiség') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="number" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
