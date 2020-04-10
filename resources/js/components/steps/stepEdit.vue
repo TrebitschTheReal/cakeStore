d9a8af6f0a23323e26670b2b80b0d41a8748a80f
Viktor Dohanyos <vorkapp@gmail.com>
2020-04-10 21:03:34 +0200

    update - close the day

--- resources/js/components/steps/stepEdit.vue
+++ resources/js/components/steps/stepEdit.vue
@@ -7,7 +7,7 @@
          <div class="row">
             <div class="col col-6-lg col-2-xs">
                <div class="form-group">
-                  <label for="recipe-name">Recept ID-ja:</label>
+                  <label for="recipe-name">Válassz receptet:</label>
                   <input required
                          type="number"
                          v-model="modifiableRecipeId"
@@ -15,7 +15,7 @@
                          aria-describedby="recipe-name"
                          placeholder="Írd be a recept id-ját">
                </div>
-               <button @click="modifyRecipe" class="col btn btn-warning">Recept módosítása</button>
+               <button @click="modifyRecipe" class="col btn btn-warning">Kiválasztott recept módosítása</button>
             </div>
          </div>
       </div>
@@ -26,15 +26,31 @@
    export default {
       name: "stepModify",
 
+      mounted() {
+         this.fetchRecipes()
+      },
+
       data: function () {
          return {
             modifiableRecipeId: null,
+            fetchedRecipes: {}
          }
       },
 
       methods: {
          modifyRecipe() {
             this.$emit('modifyRecipe', this.modifiableRecipeId)
+         },
+
+         fetchRecipes() {
+            axios.get('/cakelist')
+               .then((response) => {
+                  this.fetchedRecipes = response.data;
+                  console.log(this.fetchedRecipes);
+               })
+               .catch((error) => {
+                  console.log(error);
+               });
          }
       }
    }
