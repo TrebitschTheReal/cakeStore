067a6280b527c66f6c36ad0f8c0a918dbf07e6a3
Viktor Dohanyos <dohanyos.viktor@innostart.hu>
2020-06-08 15:24:19 +0200

    refactoring

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
         $this->call(CakeTableSeeder::class);
         $this->call(IngredientSeeder::class);
         $this->call(RequiredIngredientsSeeder::class);
-        $this->call(RoleTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(UnitsSeeder::class);
     }
 }
 
 /* Használaton kívül:
- * $this->call(UsersTableSeeder::class);
+ * $this->call(DummyUsersTableSeeder::class);
  */
