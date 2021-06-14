41a984d43df4d2d052136aaad160e52bce94566c
Viktor Dohányos <vorkapp@gmail.com>
2021-06-14 09:46:07 +0200

    Demo update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
    <div class="container">
-      <div class="row justify-content-center text-center">
+      <div class="row justify-content-center">
          <div class="col">
-            <h1 class="">Üdvözlünk a CakeStore oldalán!</h1>
+            <h1 class="text-center">Üdvözlünk a CakeStore oldalán!</h1>
             <hr class="">
-            <p class="">Kérjük regisztrálj a felületre annak használatához, vagy ha már rendelkezel fiókkal, akkor lépj
+            <p class="text-center">Kérjük regisztrálj a felületre annak használatához, vagy ha már rendelkezel fiókkal, akkor lépj
                be!</p>
             <div class="mt-5 alert alert-info">
-               <div class="col-7 text-center mx-auto">
-                  <p class="font-weight-bold h5">A webalkalmazás demo üzemmódban fut!</p>
+               <div class="col-7 mx-auto">
+                  <p class="font-weight-bold h5 text-center">A webalkalmazás demo üzemmódban fut!</p>
                   <p>Regisztrációt követően rögtön admin jogosultságot kapsz, amivel a oldal összes funkcióját elérheted
                      és kipróbálhatod.</p>
                   <p>Ez egy recept építő / alapanyag managelő alkalmazás, ahol a bejelentkezést követően:</p>
@@ -20,9 +20,8 @@
                      <li>interaktív keresővel listázhatod a recepteket</li>
                      <li>egy recept elkészítési költségét alapanyagra és mennyiségre lebontva láthatod</li>
                   </ul>
-                  <p>Az adatbázist nem tudod tönkretenni, mert minden óra 00. percében visszállnak az
-                     alapértelmezett adatok, tehát nyugodtan módosíts, törölj bármit amit szeretnél.</p>
-                  <p>Jó szórakozást :)</p>
+                  <p class="font-weight-bold float-left">Az adatbázis tartalma minden óra 00. percében törlődik és visszaáll alapértelmezett értékekre, tehát nyugodtan módosíts, törölj bármit amit szeretnél.</p>
+                  <p class="text-center">Jó szórakozást :)</p>
                </div>
             </div>
          </div>
